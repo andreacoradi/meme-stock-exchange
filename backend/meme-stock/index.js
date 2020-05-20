de2190e950c2a13766aa5f016198e1640175a688
Andrea Coradi <andreacoradi30@gmail.com>
2020-05-20 16:41:10 +0200

    Aggiunto getUsername e getUserID

--- backend/meme-stock/index.js
+++ backend/meme-stock/index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const mysql = require("promise-mysql")
 require('dotenv').config()
+const fetch = require("node-fetch")
+
+const AUTH_URL = "https://auth-go.rover.acoradi.xyz/api/v1"
 
 
 const app = express()
@@ -33,21 +36,33 @@ const getMemeValue = async (memeID) => {
     return database.query(sql, values)
 }
 
-const getUser = async (memeID) => {
-    let sql = "SELECT score FROM memes WHERE name = ?"
-    let values = ["id", memeID]
-    // TODO prepare sql
-    return database.query(sql, values)
+const getUsername = async (token) => {
+    const result = await fetch(`${AUTH_URL}/auth`, {
+        headers: {
+            "Authorization": "Bearer " + token
+        }
+    })
+    const json = await result.json()
+    if(json.username) {
+        return json.username
+    } else {
+        throw json.message
+    }
 }
 
-const buyMeme = async (memeID, quantita) => {
+const getUserID = async (username) => {
+    const sql = "SELECT id FROM user WHERE username = ?"
+    return database.query(sql, [username])
+}
+
+const buyMeme = async (memeID, userID, quantita) => {
     try {
         const valoreMeme = (await getMemeValue(memeID))[0].score
 
         console.log("VALORE MEME", valoreMeme);
         sql = "INSERT INTO investment (id_meme, id_utente, quantita, valore_meme) VALUES (?, ?, ?, ?)"
-        // TODO ottieni l'id dell'utente
-        values = [memeID, 1, quantita, valoreMeme]
+
+        values = [memeID, userID, quantita, valoreMeme]
         return database.query(sql, values)
     } catch (error) {
         console.error(error);
@@ -63,7 +78,7 @@ app.get("/memes", async (req, res) => {
     }
 })
 
-app.post("/memes/:id", (req, res) => {
+app.post("/memes/:id", async (req, res) => {
     console.log(req.params.id);
     const id = req.params.id
     const auth = req.headers["authorization"]
@@ -73,6 +88,7 @@ app.post("/memes/:id", (req, res) => {
         return
     }
     const token = auth.split(" ")[1]
+
     const { action, quantity } = req.body
     console.log(token);
     console.log(action, quantity);
@@ -82,15 +98,32 @@ app.post("/memes/:id", (req, res) => {
         return
     } 
 
-    if(action === "buy") {
-        buyMeme(id, quantity)
-    } else if (action === "sell") {
-        sellMeme(id)
+    try {
+        const username = await getUsername(token)
+        let userID = await getUserID(username)
+        userID = userID[0].id
+
+        if(userID.length === 0) {
+            res.send({
+                "message": "no user found"
+            })
+            return
+        }
+
+        if(action === "buy") {
+            buyMeme(id, userID, quantity)
+        } else if (action === "sell") {
+            sellMeme(id, userID, quantity)
+        }
+        res.send({
+            "message": "si, tutto a posto!"
+        })
+    } catch (error) {
+        throw error
     }
 
-    res.send({
-        "message": "si, tutto a posto!"
-    })
+
+
 })
 
 app.listen(1337)
