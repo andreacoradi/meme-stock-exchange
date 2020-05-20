e633faa9b1b34266bded944d3f6633c05fedf53e
Andrea Coradi <andreacoradi30@gmail.com>
2020-05-20 20:49:13 +0200

    ?

--- backend/meme-stock/index.js
+++ backend/meme-stock/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const mysql = require("promise-mysql")
 require('dotenv').config()
 const fetch = require("node-fetch")
-const e = require("express")
+const express = require("express")
 
 const AUTH_URL = "https://auth-go.rover.acoradi.xyz/api/v1"
 
