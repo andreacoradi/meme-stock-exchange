1c26386dd31eb0c33e7e07f4f507d0cfe2c8027d
Matteo-x220 <matteosasoris@gmail.com>
2020-05-30 06:26:17 +0200

    Capped username to sub-20 characters during signup

--- frontend/src/components/Vault.js
+++ frontend/src/components/Vault.js
@@ -1,21 +1,21 @@
-import React from "react"
-import ScrollTop from 'react-scrolltop-button';
+import React from 'react'
+import ScrollTop from 'react-scrolltop-button'
 // import MaterialCard from "./MaterialCard"
-import { MemeList } from "./MemeList"
-import { Searchbar } from "./Searchbar"
+import { MemeList } from './MemeList'
+// import { Searchbar } from "./Searchbar"
 
 export const Vault = () => {
   return (
-    <div className="d-flex flex-column">
-      <div className="d-flex justify-content-center flex-grow-1 bd-highlight">
+    <div className='d-flex flex-column'>
+      <div className='d-flex justify-content-center flex-grow-1 bd-highlight'>
         {/* <Searchbar /> */}
-        <p id="fillingSpace"></p>
+        <p id='fillingSpace'></p>
         {/* {moneyCount} */}
       </div>
-      <div className="d-flex justify-content-center">
+      <div className='d-flex justify-content-center'>
         <MemeList
-          requestType="user_owned"
-          count=""
+          requestType='user_owned'
+          count=''
           scroll={false}
           sell={true}
         />
